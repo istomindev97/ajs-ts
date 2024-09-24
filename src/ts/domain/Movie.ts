6ad8402c1cab3b5e9e99646e5b8ba28328cb034b
Ilya Istomin <istominilya97@gmail.com>
2024-09-24 15:36:57 +0300

    classes changed

--- src/ts/domain/Movie.ts
+++ src/ts/domain/Movie.ts
@@ -1,20 +1,25 @@
 import Buyable from './Buyable';
 
 export default class Movie implements Buyable {
-    constructor(
-        readonly id: number,
-        readonly price: number, 
-        readonly name: string,
-        readonly author: string,
-        readonly year: number,
-        readonly country: string,
-        readonly slogan: string,
-        readonly genre: string,
-        readonly time: number,
-    )
-     { }
+    readonly id: number;
+    readonly price: number;
+    readonly name: string;
+    readonly author: string;
+    readonly year: number;
+    readonly country: string;
+    readonly slogan: string;
+    readonly genre: string;
+    readonly time: number;
+
+    constructor(id: number, price: number, name: string, author: string, year: number, country: string, slogan: string, genre: string, time: number) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.year = year;
+        this.country = country;
+        this.slogan = slogan;
+        this.genre = genre;
+        this.time = time;
+     }
 }
-
-
-
-
--- src/ts/domain/MusicAlbum.ts
+++ src/ts/domain/MusicAlbum.ts
@@ -1,10 +1,15 @@
 import Buyable from './Buyable';
 
 export default class MusicAlbum implements Buyable {
-    constructor(
-        readonly id: number,
-        readonly name: string,
-        readonly author: string,
-        readonly price: number,
-    ) { }
+    readonly id: number;
+    readonly name: string;
+    readonly author: string;
+    readonly price: number;
+    
+    constructor(id: number, name: string, author: string, price: number) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
 }
